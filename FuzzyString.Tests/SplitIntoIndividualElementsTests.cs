26122e33f3c1b456297f39e437da162e8881ab9b
James Manning <james.manning@gmail.com>
2015-05-08 15:11:53 -0400

    tests for SplitIntoIndividualElements

---
+++ FuzzyString.Tests/SplitIntoIndividualElementsTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FuzzyString.Tests
+{
+    [TestClass]
+    public class SplitIntoIndividualElementsTests
+    {
+        [TestMethod]
+        public void WhenSourceIsEmpty_ShouldReturnEmptyArray()
+        {
+            var result = Operations.SplitIntoIndividualElements("");
+            Assert.AreEqual(0, result.Length);
+        }
+
+        [TestMethod]
+        public void WhenSourceIsOneCharacter_ShouldReturnArrayWithOneString()
+        {
+            var result = Operations.SplitIntoIndividualElements("1");
+            CollectionAssert.AreEqual(new[] { "1" }, result);
+        }
+
+        [TestMethod]
+        public void WhenSourceIsFiveCharacters_ShouldReturnArrayWithFiveStrings()
+        {
+            var result = Operations.SplitIntoIndividualElements("12345");
+            CollectionAssert.AreEqual(new[] { "1", "2", "3", "4", "5" }, result);
+        }
+    }
+}
