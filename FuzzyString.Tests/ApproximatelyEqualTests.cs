64101fdbac2132e89aff708bbebda7a5dec529fe
James Manning <james.manning@gmail.com>
2015-05-11 17:46:40 -0400

    switch to data-driven tests to help make it easier to understand

--- FuzzyString.Tests/ApproximatelyEqualTests.cs
+++ FuzzyString.Tests/ApproximatelyEqualTests.cs
@@ -2,6 +2,7 @@
 
 using System.Collections.Generic;
 using Xunit;
+using Xunit.Extensions;
 
 namespace FuzzyString.Tests
 {
@@ -9,7 +10,7 @@ namespace FuzzyString.Tests
     public class ApproximatelyEqualTests
     {
         [Fact]
-        public void WhenSimilarString_ShouldReturnTrueForWeakAndNormalAndStrong()
+        public void WhenSimilarString_AndMultipleComparisonOptions_ShouldReturnTrueForWeakAndNormalAndStrong()
         {
             const string kevin = "kevin";
             const string kevyn = "kevyn";
@@ -64,100 +65,66 @@ namespace FuzzyString.Tests
             Assert.False(kevin.ApproximatelyEquals(kevyn, options, (FuzzyStringComparisonTolerance)999));
         }
 
-        [Fact]
-        public void WhenSimilarString_ShouldReturnTrueForJaroDistanceAndWeakAndNormal_FalseForStrong()
+        [Theory]
+        [InlineData(FuzzyStringComparisonOptions.UseHammingDistance              , FuzzyStringComparisonTolerance.Weak  , true )]
+        [InlineData(FuzzyStringComparisonOptions.UseHammingDistance              , FuzzyStringComparisonTolerance.Normal, true )]
+        [InlineData(FuzzyStringComparisonOptions.UseHammingDistance              , FuzzyStringComparisonTolerance.Strong, true )]
+
+        [InlineData(FuzzyStringComparisonOptions.UseJaccardDistance              , FuzzyStringComparisonTolerance.Weak  , true )]
+        [InlineData(FuzzyStringComparisonOptions.UseJaccardDistance              , FuzzyStringComparisonTolerance.Normal, true )]
+        [InlineData(FuzzyStringComparisonOptions.UseJaccardDistance              , FuzzyStringComparisonTolerance.Strong, false)]
+
+        [InlineData(FuzzyStringComparisonOptions.UseJaroDistance                 , FuzzyStringComparisonTolerance.Weak  , true )]
+        [InlineData(FuzzyStringComparisonOptions.UseJaroDistance                 , FuzzyStringComparisonTolerance.Normal, true )]
+        [InlineData(FuzzyStringComparisonOptions.UseJaroDistance                 , FuzzyStringComparisonTolerance.Strong, false)]
+
+        [InlineData(FuzzyStringComparisonOptions.UseJaroWinklerDistance          , FuzzyStringComparisonTolerance.Weak  , true )]
+        [InlineData(FuzzyStringComparisonOptions.UseJaroWinklerDistance          , FuzzyStringComparisonTolerance.Normal, false)]
+        [InlineData(FuzzyStringComparisonOptions.UseJaroWinklerDistance          , FuzzyStringComparisonTolerance.Strong, false)]
+
+        [InlineData(FuzzyStringComparisonOptions.UseLevenshteinDistance          , FuzzyStringComparisonTolerance.Weak  , false)]
+        [InlineData(FuzzyStringComparisonOptions.UseLevenshteinDistance          , FuzzyStringComparisonTolerance.Normal, false)]
+        [InlineData(FuzzyStringComparisonOptions.UseLevenshteinDistance          , FuzzyStringComparisonTolerance.Strong, false)]
+
+        [InlineData(FuzzyStringComparisonOptions.UseLongestCommonSubsequence     , FuzzyStringComparisonTolerance.Weak  , true )]
+        [InlineData(FuzzyStringComparisonOptions.UseLongestCommonSubsequence     , FuzzyStringComparisonTolerance.Normal, true )]
+        [InlineData(FuzzyStringComparisonOptions.UseLongestCommonSubsequence     , FuzzyStringComparisonTolerance.Strong, true )]
+
+        [InlineData(FuzzyStringComparisonOptions.UseLongestCommonSubstring       , FuzzyStringComparisonTolerance.Weak  , true )]
+        [InlineData(FuzzyStringComparisonOptions.UseLongestCommonSubstring       , FuzzyStringComparisonTolerance.Normal, true )]
+        [InlineData(FuzzyStringComparisonOptions.UseLongestCommonSubstring       , FuzzyStringComparisonTolerance.Strong, false)]
+
+        [InlineData(FuzzyStringComparisonOptions.UseNormalizedLevenshteinDistance, FuzzyStringComparisonTolerance.Weak  , true )]
+        [InlineData(FuzzyStringComparisonOptions.UseNormalizedLevenshteinDistance, FuzzyStringComparisonTolerance.Normal, true )]
+        [InlineData(FuzzyStringComparisonOptions.UseNormalizedLevenshteinDistance, FuzzyStringComparisonTolerance.Strong, true )]
+
+        [InlineData(FuzzyStringComparisonOptions.UseOverlapCoefficient           , FuzzyStringComparisonTolerance.Weak  , true )]
+        [InlineData(FuzzyStringComparisonOptions.UseOverlapCoefficient           , FuzzyStringComparisonTolerance.Normal, true )]
+        [InlineData(FuzzyStringComparisonOptions.UseOverlapCoefficient           , FuzzyStringComparisonTolerance.Strong, true )]
+
+        [InlineData(FuzzyStringComparisonOptions.UseRatcliffObershelpSimilarity  , FuzzyStringComparisonTolerance.Weak  , true )]
+        [InlineData(FuzzyStringComparisonOptions.UseRatcliffObershelpSimilarity  , FuzzyStringComparisonTolerance.Normal, true )]
+        [InlineData(FuzzyStringComparisonOptions.UseRatcliffObershelpSimilarity  , FuzzyStringComparisonTolerance.Strong, true )]
+
+        [InlineData(FuzzyStringComparisonOptions.UseSorensenDiceDistance         , FuzzyStringComparisonTolerance.Weak  , true )]
+        [InlineData(FuzzyStringComparisonOptions.UseSorensenDiceDistance         , FuzzyStringComparisonTolerance.Normal, true )]
+        [InlineData(FuzzyStringComparisonOptions.UseSorensenDiceDistance         , FuzzyStringComparisonTolerance.Strong, true )]
+
+        [InlineData(FuzzyStringComparisonOptions.UseTanimotoCoefficient          , FuzzyStringComparisonTolerance.Weak  , true )]
+        [InlineData(FuzzyStringComparisonOptions.UseTanimotoCoefficient          , FuzzyStringComparisonTolerance.Normal, true )]
+        [InlineData(FuzzyStringComparisonOptions.UseTanimotoCoefficient          , FuzzyStringComparisonTolerance.Strong, false)]
+        public void WhenSimilarString_ShouldReturnExpectedValue(FuzzyStringComparisonOptions fuzzyStringComparisonOption, FuzzyStringComparisonTolerance fuzzyStringComparisonTolerance, bool expectedValue)
         {
             const string kevin = "kevin";
             const string kevyn = "kevyn";
 
             var options = new List<FuzzyStringComparisonOptions>
             {
-                FuzzyStringComparisonOptions.UseJaroDistance,
+                fuzzyStringComparisonOption,
             };
 
-            Assert.True(kevin.ApproximatelyEquals(kevyn, options, FuzzyStringComparisonTolerance.Weak));
-            Assert.True(kevin.ApproximatelyEquals(kevyn, options, FuzzyStringComparisonTolerance.Normal));
-            Assert.False(kevin.ApproximatelyEquals(kevyn, options, FuzzyStringComparisonTolerance.Strong));
-        }
-
-        [Fact]
-        public void WhenSimilarString_ShouldReturnTrueForJaroWinklerDistanceAndWeak_FalseForNormalAndStrong()
-        {
-            const string kevin = "kevin";
-            const string kevyn = "kevyn";
-
-            var options = new List<FuzzyStringComparisonOptions>
-            {
-                FuzzyStringComparisonOptions.UseJaroWinklerDistance,
-            };
-
-            Assert.True(kevin.ApproximatelyEquals(kevyn, options, FuzzyStringComparisonTolerance.Weak));
-            Assert.False(kevin.ApproximatelyEquals(kevyn, options, FuzzyStringComparisonTolerance.Normal));
-            Assert.False(kevin.ApproximatelyEquals(kevyn, options, FuzzyStringComparisonTolerance.Strong));
-        }
-
-        [Fact]
-        public void WhenSimilarString_ShouldReturnFalseForLevenshteinDistanceAndWeakAndNormalAndStrong()
-        {
-            const string kevin = "kevin";
-            const string kevyn = "kevyn";
-
-            var options = new List<FuzzyStringComparisonOptions>
-            {
-                FuzzyStringComparisonOptions.UseLevenshteinDistance,
-            };
-
-            Assert.False(kevin.ApproximatelyEquals(kevyn, options, FuzzyStringComparisonTolerance.Weak));
-            Assert.False(kevin.ApproximatelyEquals(kevyn, options, FuzzyStringComparisonTolerance.Normal));
-            Assert.False(kevin.ApproximatelyEquals(kevyn, options, FuzzyStringComparisonTolerance.Strong));
-        }
-
-        [Fact]
-        public void WhenSimilarString_ShouldReturnTrueForLongestCommonSubstringAndAndWeakAndNormal_FalseForStrong()
-        {
-            const string kevin = "kevin";
-            const string kevyn = "kevyn";
-
-            var options = new List<FuzzyStringComparisonOptions>
-            {
-                FuzzyStringComparisonOptions.UseLongestCommonSubstring,
-            };
-
-            Assert.True(kevin.ApproximatelyEquals(kevyn, options, FuzzyStringComparisonTolerance.Weak));
-            Assert.True(kevin.ApproximatelyEquals(kevyn, options, FuzzyStringComparisonTolerance.Normal));
-            Assert.False(kevin.ApproximatelyEquals(kevyn, options, FuzzyStringComparisonTolerance.Strong));
-        }
-
-        [Fact]
-        public void WhenSimilarString_ShouldReturnTrueForSorensenDiceDistanceAndWeakAndNormalAndStrong()
-        {
-            const string kevin = "kevin";
-            const string kevyn = "kevyn";
-
-            var options = new List<FuzzyStringComparisonOptions>
-            {
-                FuzzyStringComparisonOptions.UseSorensenDiceDistance,
-            };
-
-            Assert.True(kevin.ApproximatelyEquals(kevyn, options, FuzzyStringComparisonTolerance.Weak));
-            Assert.True(kevin.ApproximatelyEquals(kevyn, options, FuzzyStringComparisonTolerance.Normal));
-            Assert.True(kevin.ApproximatelyEquals(kevyn, options, FuzzyStringComparisonTolerance.Strong));
-        }
-
-        [Fact]
-        public void WhenSimilarString_ShouldReturnTrueForRatcliffObershelpSimilarityAndWeakAndNormalAndStrong()
-        {
-            const string kevin = "kevin";
-            const string kevyn = "kevyn";
-
-            var options = new List<FuzzyStringComparisonOptions>
-            {
-                FuzzyStringComparisonOptions.UseRatcliffObershelpSimilarity,
-            };
-
-            Assert.True(kevin.ApproximatelyEquals(kevyn, options, FuzzyStringComparisonTolerance.Weak));
-            Assert.True(kevin.ApproximatelyEquals(kevyn, options, FuzzyStringComparisonTolerance.Normal));
-            Assert.True(kevin.ApproximatelyEquals(kevyn, options, FuzzyStringComparisonTolerance.Strong));
+            var result = kevin.ApproximatelyEquals(kevyn, options, fuzzyStringComparisonTolerance);
+            Assert.Equal(expectedValue, result);
         }
     }
 }
